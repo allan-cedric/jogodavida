cf1361503843542160eac2d6ae4c9e8e027e7c75
allan-cedric <allan.py3000@gmail.com>
2020-04-24 16:18:09 -0300

    Lista duplamente encadeada com 2 CHAVES

---
+++ lista_dupla.h
@@ -0,0 +1,132 @@
+/* 
+  Header file : 'lista_dupla.h'
+  Escrito por : Allan Cedric G.B. Alves da Silva
+  Profile : Aluno de graduação do curso de Ciência da Computação (UFPR)
+  GRR : 20190351
+*/
+
+#ifndef __LISTA_DUPLA_H__
+#define __LISTA_DUPLA_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct t_nodo
+{
+  int i, j;
+  struct t_nodo *prox;
+  struct t_nodo *prev;
+};
+typedef struct t_nodo t_nodo;
+
+struct t_lista
+{
+  t_nodo *ini;
+  t_nodo *atual;
+  t_nodo *fim;
+  int tamanho;
+};
+typedef struct t_lista t_lista;
+
+/*
+  Cria uma lista vazia. Retorna 1 se a operação foi bem sucedida, senão 0.
+*/
+int inicializa_lista(t_lista *l);
+
+/*
+  Retorna 1 se a lista está vazia e zero caso contrário.
+*/
+int lista_vazia(t_lista *l);
+
+/*
+  Retorna 1 se a lista está destruida e zero caso contrário
+*/
+int lista_destruida(t_lista *l);
+
+/*
+  Remove todos os elementos da lista e faz com que ela aponte para NULL.
+*/
+void destroi_lista(t_lista *l);
+
+/*
+  Insere o elemento item no início da lista.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int insere_inicio_lista(int i, int j, t_lista *l);
+
+/*
+  Retorna o tamanho da lista em *tam.
+*/
+void tamanho_lista(int *tam, t_lista *l);
+
+/*
+  Insere o elemento item no final da lista.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int insere_fim_lista(int i, int j, t_lista *l);
+
+/*
+  Remove o primeiro elemento da lista e o retorna em *item.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int remove_inicio_lista(int *i, int *j, t_lista *l);
+
+/*
+  Remove o último elemento da lista e o retorna em *item.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int remove_fim_lista(int *i, int *j, t_lista *l);
+
+/*
+  Se o elemento chave existir na lista, o retorna em *item.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário
+  (elemento não encontrado também retorna zero).
+*/
+int remove_item_lista(int chave, int *item, t_lista *l);
+
+/*
+  Retorna 1 se o elemento contendo a chave chave existe na lista,
+  caso contrário retorna zero.
+*/
+int pertence_lista(int i, int j, t_lista *l);
+
+/* 
+  Inicializa o ponteiro atual para o primeiro elemento da lista.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int inicializa_atual_inicio(t_lista *l);
+
+/* 
+  Inicializa o ponteiro atual para o ultimo elemento da lista.
+  Retorna 1 se a operação foi bem sucedida e zero caso contrário.
+*/
+int inicializa_atual_fim(t_lista *l);
+
+/*
+  Faz o ponteiro atual apontar para o próximo nodo da lista l e retorna 
+  este ponteiro. Se atual estiver apontando para o último, isto é, não 
+  tem próximo, retorna NULL.
+*/
+void incrementa_atual(t_lista *l);
+
+/*
+  Faz o ponteiro atual apontar para o nodo anterior da lista l e retorna 
+  este ponteiro. Se atual estiver apontando para o primeiro, isto é, não 
+  tem anterior, retorna NULL.
+*/
+void decrementa_atual(t_lista *l);
+
+/*
+  Retorna em *item o valor contido na chave apontada pelo ponteiro atual. 
+  Se atual não for válido a função retorna zero senão retorna 1.
+*/
+int consulta_item_atual(int *i, int *j, t_lista *atual);
+
+/*
+  Remove o elemento apontado por atual da lista l e o retorna em *item.
+  Faz o atual apontar para o sucessor do nodo removido.
+  Retorna 1 se houve sucesso e zero caso contrário.
+*/
+int remove_item_atual(int *i, int *j, t_lista *l);
+
+#endif
